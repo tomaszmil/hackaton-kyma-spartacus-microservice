a78a4252ba73c151bae6f4d6c389e98761bf6588
Miler, Tomasz <tomasz.miler@sap.com>
2019-06-05 17:44:23 +0200

    CORS

--- src/main/java/purchase/controller/OrderController.java
+++ src/main/java/purchase/controller/OrderController.java
@@ -2,6 +2,7 @@ package purchase.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,8 @@ import purchase.service.OrderService;
 
 import java.util.List;
 
+
+@CrossOrigin(origins = "*")
 @RestController
 public class OrderController {
 
